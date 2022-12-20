635a3d1bf6acee2c31bad98e9fd8bd43ec6bf06c
liyulu666 <73726245+liyulu666@users.noreply.github.com>
2022-12-20 19:01:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'otagU5_X5L2MdaHPnyqZqcM4wlwQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3tdvbvisKveR8oQO_4dkTkxP0EBQ3tdu4vsoj8fNh7Y',
+      useTemplateId: '1eXjjBWj6VhG0n-_0SwkBObdtbHSkqYyYvEK4s6GLJo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-13',
       festivals: [
@@ -44,7 +44,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2021-02-01' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'birthday_day', date: '2022-09-09' },
       ],
     },
   ],
